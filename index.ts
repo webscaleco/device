47c9c34ba3f0ce5086290833177d29fd03d962ba
Jeremy Foster <jeremy.foster@live.com>
2016-09-14 10:50:54 -0700

    don't send msgs until client open We have to put the actual sending of the messages inside the client.open callback so it doesn't happen until the client actually gets connected

--- index.ts
+++ index.ts
@@ -10,9 +10,9 @@ let Message = require('azure-iot-device').Message;
 let clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
 
 //We will have multiple devices in simulation passed in the -d argument for device number (ex: waterrower1) 1-12 are currently on IoT hub
-let device = args["d"] ;
-if (config.has(device)){
-  var deviceConnectionString = config.get(device);
+let device = args["d"];
+if (config.has(device)) {
+    var deviceConnectionString = config.get(device);
 }
 
 let client = clientFromConnectionString(deviceConnectionString);
@@ -30,39 +30,42 @@ console.log(`Using ${name} as rower name.`);
 console.log(`Attempting to connect to ${socketServerUrl}`);
 
 //open connection to iothub
-client.open(err => console.log(err ? 'Could not connect: ' + err : 'Client connected'));
+client.open(err => {
+    console.log(err ? 'Could not connect: ' + err : 'Client connected');
 
-//wire up to the socket server
-var socket = io(socketServerUrl);
-// socket.on("message", data => {
-//     if (data.message == 'startrace') {
-        waterrower.reset();
-        // waterrower.defineDistanceWorkout(data.distance);
-        if(simulationMode) waterrower.startSimulation();
-//     }
-// });
-//respond to the waterrower sending data
-waterrower.datapoints$.subscribe(() => {
-    let msg = {
-        message: "strokedata",
-        name: name,
-        ms_distance: waterrower.readDataPoint('ms_distance'),
-        m_s_total: waterrower.readDataPoint('m_s_total')/100, //convert cm to m
-        m_s_average: waterrower.readDataPoint('m_s_average')/100, //convert cm to m
-        total_kcal: waterrower.readDataPoint('total_kcal')/1000
-    };
-    console.log(msg);
-    socket.send(msg);
-    //ISSUE05: send via iothub instead of sockets
-    var data = JSON.stringify({ deviceId: device, msg });
-    let message = new Message(JSON.stringify(data));
-    client.sendEvent(message, printResultFor('send'));
-
-    //for testing
-        function printResultFor(op) {
-        return function printResult(err, res) {
-            if (err) console.log(op + ' error: ' + err.toString());
-            if (res) console.log(op + ' status: ' + res.constructor.name);
+    //wire up to the socket server
+    var socket = io(socketServerUrl);
+    // socket.on("message", data => {
+    //     if (data.message == 'startrace') {
+    waterrower.reset();
+    // waterrower.defineDistanceWorkout(data.distance);
+    if (simulationMode) waterrower.startSimulation();
+    //     }
+    // });
+    //respond to the waterrower sending data
+    waterrower.datapoints$.subscribe(() => {
+        let msg = {
+            message: "strokedata",
+            name: name,
+            ms_distance: waterrower.readDataPoint('ms_distance'),
+            m_s_total: waterrower.readDataPoint('m_s_total') / 100, //convert cm to m
+            m_s_average: waterrower.readDataPoint('m_s_average') / 100, //convert cm to m
+            total_kcal: waterrower.readDataPoint('total_kcal') / 1000
         };
-    }
+        console.log(msg);
+        socket.send(msg);
+        //ISSUE05: send via iothub instead of sockets
+        var data = JSON.stringify({ deviceId: device, msg });
+        let message = new Message(JSON.stringify(data));
+        client.sendEvent(message, printResultFor('send'));
+
+        //for testing
+        function printResultFor(op) {
+            return function printResult(err, res) {
+                if (err) console.log(op + ' error: ' + err.toString());
+                if (res) console.log(op + ' status: ' + res.constructor.name);
+            };
+        }
+    });
 });
+

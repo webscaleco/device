bd281b1964eb23a0397759422c71f32109d10579
Jeremy Foster <jeremy.foster@live.com>
2016-09-21 08:45:17 -0700

    Add autoStart parameter Allows us to tell the process to start the simulation as soon as it starts

--- index.ts
+++ index.ts
@@ -19,40 +19,60 @@ let client = clientFromConnectionString(deviceConnectionString);
 
 import { WaterRower } from 'waterrower';
 
-let waterrower = new WaterRower();
+let waterrower = new WaterRower({ datapoints: ['ms_distance', 'm_s_total', 'm_s_average', 'total_kcal'] });
 
 //command line arguments
 let name = args["n"] || args["name"] || (config.has('name') ? config.get('name') : undefined) || 'Rower';
 let socketServerUrl = args["s"] || args["socket-server-url"] || (config.has('socketServerUrl') ? config.get('socketServerUrl') : undefined) || 'http://localhost:8080';
 let simulationMode = args["m"] || args["simulation-mode"] || (config.has('simulationMode') ? config.get('simulationMode') : undefined);
+let autoStart = args["a"] || args["auto-start"] || (config.has('autoStart') ? config.get('autoStart') : false);
 
 console.log(`Using ${name} as rower name.`);
 console.log(`Attempting to connect to ${socketServerUrl}`);
+if (simulationMode) console.log('This Regatta machine is running in simulation mode.');
 
 //open connection to iothub
 client.open(err => {
-    if(err) throw(`Error connecting to the Regatta service. Please check your connection. [${err}]`);
+    if (err) throw (`Error connecting to the Regatta service. Please check your connection. [${err}]`);
 
     //wire up to the socket server
     var socket = io(socketServerUrl);
-    // socket.on("message", data => {
-    //     if (data.message == 'startrace') {
-    waterrower.reset();
-    // waterrower.defineDistanceWorkout(data.distance);
-    if (simulationMode) waterrower.startSimulation();
-    //     }
-    // });
+    socket.on('connect', () => {
+        //send a check-in message so the rower can be added to the list
+        socket.send({ message: 'rower-checkin', name: name });
+    });
+
+    if (autoStart) start(150);
+
+    socket.on("message", data => {
+        if (data.message == 'session-start') start(data.distance);
+    });
+
+    function start(distance: number) {
+        waterrower.reset();
+        waterrower.defineDistanceWorkout(distance);
+        if (simulationMode) waterrower.startSimulation();
+    }
+
+    let messageCount = 0;
+
     //respond to the waterrower sending data
     waterrower.datapoints$.subscribe(() => {
+        if (messageCount > 0) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);  // move cursor to beginning of line
+        }
+        messageCount++;
+        let values = waterrower.readDataPoints(['ms_distance', 'm_s_total', 'm_s_average', 'total_kcal']);
         let msg = {
             message: "strokedata",
             name: name,
-            ms_distance: waterrower.readDataPoint('ms_distance'),
-            m_s_total: waterrower.readDataPoint('m_s_total') / 100, //convert cm to m
-            m_s_average: waterrower.readDataPoint('m_s_average') / 100, //convert cm to m
-            total_kcal: waterrower.readDataPoint('total_kcal') / 1000
+            ms_distance: values['ms_distance'],
+            m_s_total: values['m_s_total'] / 100, //convert cm to m
+            m_s_average: values['m_s_average'] / 100, //convert cm to m
+            total_kcal: values['total_kcal'] / 1000 //convert to calories
         };
-        console.log(msg);
+        process.stdout.write(`Messages sent: ${messageCount}`);  // write text
         socket.send(msg);
         //ISSUE05: send via iothub instead of sockets
         var data = JSON.stringify({ deviceId: device, msg });
@@ -68,4 +88,3 @@ client.open(err => {
         }
     });
 });
-

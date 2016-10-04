bfc850230da5b3712bf56556aaa9851fad01c9bf
Jeremy Foster <jeremy.foster@live.com>
2016-10-04 09:14:04 -0700

    organize config vars, add logging, organized code

--- index.ts
+++ index.ts
@@ -2,69 +2,65 @@ import { Observable, Subject } from 'rxjs/Rx';
 import * as config from 'config';
 import * as minimist from 'minimist';
 import * as io from 'socket.io-client';
-
-var args = minimist(process.argv.slice(2));
-
-//IoThub requires
-let Message = require('azure-iot-device').Message;
-let clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
-
-//We will have multiple devices in simulation passed in the -d argument for device number (ex: waterrower1) 1-12 are currently on IoT hub
-let device = args["d"];
-if (config.has(device)) {
-    var deviceConnectionString = config.get(device);
-}
-
-let client = clientFromConnectionString(deviceConnectionString);
-
 import { WaterRower } from 'waterrower';
+import * as azureIotDevice from 'azure-iot-device';
+import * as azureIotDeviceAmqp from 'azure-iot-device-amqp';
 
-let waterrower = new WaterRower({ datapoints: ['ms_distance', 'm_s_total', 'm_s_average', 'total_kcal'] });
+enum LogLevel { Debug, Information, Warning, Error, Fatal }
+let logLevel: LogLevel = LogLevel.Information;
 
 //command line arguments
+let args = minimist(process.argv.slice(2));
 let name = args["n"] || args["name"] || (config.has('name') ? config.get('name') : undefined) || 'Rower';
 let socketServerUrl = args["s"] || args["socket-server-url"] || (config.has('socketServerUrl') ? config.get('socketServerUrl') : undefined) || 'http://localhost:8080';
 let simulationMode = args["m"] || args["simulation-mode"] || (config.has('simulationMode') ? config.get('simulationMode') : undefined);
 let autoStart = args["a"] || args["auto-start"] || (config.has('autoStart') ? config.get('autoStart') : false);
+let device = args["d"] || args["device"] || (config.has('device') ? config.get('device') : undefined);
 
-console.log(`Using ${name} as rower name.`);
-console.log(`Attempting to connect to ${socketServerUrl}`);
-if (simulationMode) console.log('This Regatta machine is running in simulation mode.');
+//setup iothub 
+//  value of device parameter determines which configured connection string to use (ex: waterrower1)
+let deviceConnectionString = config.get(device).toString();
+let Message = azureIotDevice.Message;
+let clientFromConnectionString = azureIotDeviceAmqp.clientFromConnectionString;
+let client = clientFromConnectionString(deviceConnectionString);
+
+//create waterrower
+let waterrower = new WaterRower({ datapoints: ['ms_distance', 'm_s_total', 'm_s_average', 'total_kcal'] });
+
+log(`Using ${name} as rower name.`);
+log(`Attempting to connect to ${socketServerUrl}`);
+if (simulationMode) log('This Regatta machine is running in simulation mode.');
+
+//wire up to the socket server
+var socket = io(socketServerUrl);
+socket.on('connect', () => {
+    //send a check-in message so the rower can be added to the list
+    socket.send({ message: 'rower-checkin', name: name });
+});
+
+//when we get an incoming socket message...
+socket.on("message", data => {
+
+    //if it's a session-start then start the rower
+    if (data.message == 'session-start') start(data.distance);
+});
+
+if (autoStart) start(150);
+
+//start the rower
+function start(distance: number) {
+    waterrower.reset();
+    waterrower.defineDistanceWorkout(distance);
+    if (simulationMode) waterrower.startSimulation();
+}
 
 //open connection to iothub
 client.open(err => {
     if (err) throw (`Error connecting to the Regatta service. Please check your connection. [${err}]`);
 
-    //wire up to the socket server
-    var socket = io(socketServerUrl);
-    socket.on('connect', () => {
-        //send a check-in message so the rower can be added to the list
-        socket.send({ message: 'rower-checkin', name: name });
-    });
-
-    if (autoStart) start(150);
-
-    socket.on("message", data => {
-        if (data.message == 'session-start') start(data.distance);
-    });
-
-    function start(distance: number) {
-        waterrower.reset();
-        waterrower.defineDistanceWorkout(distance);
-        if (simulationMode) waterrower.startSimulation();
-    }
-
-    let messageCount = 0;
-
-    //respond to the waterrower sending data
+    //subscribe to the waterrower datapoints stream
     waterrower.datapoints$.subscribe(() => {
-        if (messageCount > 0) {
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);  // move cursor to beginning of line
-        }
-        messageCount++;
-        process.stdout.write(`Sending message: ${messageCount}`);  // write text
-
+        //we're only interested in four datapoints
         let values = waterrower.readDataPoints(['ms_distance', 'm_s_total', 'm_s_average', 'total_kcal']);
         let msg = {
             message: "strokedata",
@@ -79,9 +75,15 @@ client.open(err => {
         socket.send(msg);
 
         //send via iothub instead of sockets
-        let message = new Message(JSON.stringify(JSON.stringify({ deviceId: device, msg })));
+        let message = new Message(JSON.stringify({ deviceId: device, msg }));
         client.sendEvent(message, (err, res) => {
-            if (err) console.log(`Error sending to IoT Hub (${err})`);
+            if (err) log(`Error sending to IoT Hub (${err})`, LogLevel.Error);
         });
+
+        log(`Sent ${JSON.stringify(msg)}`, LogLevel.Debug);
     });
 });
+
+function log(msg: string, level: LogLevel = LogLevel.Information) {
+    if (level >= logLevel) console.log(msg);
+}
